040479f9feecf34c113884f2de552d24bc3fffe7
Andreas Fiehn <andreas@lyhnefiehn.dk>
2023-03-25 17:18:50 +0100

    Created new plot_reconstructions_function

--- src/AE_on_SERS_plotting.py
+++ src/AE_on_SERS_plotting.py
@@ -23,7 +23,7 @@ from SERS_dataset import SERSDataset
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 cuda = torch.cuda.is_available()
 
-def plot_loss(epochs, train_losses, test_losses):
+def plot_loss(train_losses, test_losses):
     """Plot the training and validation loss
     
     Args:
@@ -38,38 +38,92 @@ def plot_loss(epochs, train_losses, test_losses):
     # Set the y axis label of the current axis.
     plt.ylabel("Error")
 
-    plt.plot(np.arange(epochs), train_losses, color="black")
-    plt.plot(np.arange(epochs), test_losses, color="gray", linestyle="--")
+    n = len(train_losses)
+
+    plt.plot(np.arange(n), train_losses, color="black")
+    plt.plot(np.arange(n), test_losses, color="gray", linestyle="--")
     plt.legend(['Training error', 'Validation error'])
 
     plt.show()
-
-def plot_reconstructions(autoencoder, data_loader):
+      
+def plot_reconstructions(autoencoder, data_loader, w_h_of_subplot=(2,2)):
+    """Plot the reconstructions of the autoencoder
+    
+    Args:
+        autoencoder (nn.Module): The autoencoder
+        data_loader (DataLoader): The data loader
+        w_h_of_subplot (tuple, optional): The width and height of the subplot. Defaults to (2,2).
+    
+    """
+    w, h = w_h_of_subplot
+    j = w*h
+    plot_pos = 1
+    K = 0
     # Plot reconstructions
-    for i, (x, y) in enumerate(data_loader):
+    for i in range(j):
+        x, y = next(iter(data_loader))
+
         x_hat = autoencoder(x.to(device))
+        # print(len(x_hat))
         x_hat = x_hat.to('cpu').detach().numpy()
         x = x.to('cpu').detach().numpy()
-        y = y.to('cpu').detach().numpy()
+        print(x.shape)
 
-        for j in range(4):
-            plt.subplot(2, 2, j+1)
-            peak_pos_rounded = str(np.round(y[j],2))
-            print(peak_pos_rounded)
-            plt.plot(x[j])
-            plt.plot(x_hat[j], alpha=0.7)
+
+        # if x has more than one dimension
+        if x.shape[0] > 1:
+
+            # Loop through rows in x
+            for k in range(0, min(j,x.shape[0])):
+
+                #Get first row 
+                x_ = x[k]
+                x_hat_ = x_hat[k]
+                # flatten the data
+                x_ = x_.flatten()
+                x_hat_ = x_hat_.flatten()
+
+                plt.subplot(w, h, plot_pos)
+
+                # if isinstance(y, list):
+                #     y = y[2]
+                # peak_pos_rounded = str(list([np.round(i.tolist(),1) for i in y]))
+                plt.plot(x_)
+                plt.plot(x_hat_, alpha=0.7)
+                # plt.title(f"Peak location: {peak_pos_rounded}")
+                plt.xlabel("Wavenumber")
+                plt.ylabel("Intensity (a.u.)")
+
+                K += 1
+                plot_pos += 1
+
+                if K == j:
+                    break
+        else:
+            # flatten the data
+            x_ = x.flatten()
+            x_hat_ = x_hat.flatten()
+
+            plt.subplot(3, 2, i+1)
+
+            if isinstance(y, list):
+                y = y[2]
+            peak_pos_rounded = str(list([np.round(i.tolist(),1) for i in y]))
+            plt.plot(x_)
+            plt.plot(x_hat_, alpha=0.7)
             plt.title(f"Peak location: {peak_pos_rounded}")
             plt.xlabel("Wavenumber")
             plt.ylabel("Intensity (a.u.)")
-            
-            # break
-        # Make plot title
-        plt.suptitle(f"Reconstruction of SERS spectra")
-        # show legend   
-        plt.legend(['Original', 'Reconstruction'])
-        plt.tight_layout()
-        plt.show()
-        break
+
+        if K == j:
+            break
+        # break
+    # Make plot title
+    plt.suptitle(f"Reconstruction of SERS spectra")
+    # show legend   
+    plt.legend(['Original', 'Reconstruction'])
+    plt.tight_layout()
+    plt.show()
 
 def plot_latent_space(autoencoder, data_loader):
 
@@ -91,6 +145,28 @@ def plot_latent_space(autoencoder, data_loader):
     plt.tight_layout()
     plt.show()
    
+
+
+def plot_latent_space(autoencoder, data_loader):
+
+    # Loop through points in dataset and plot them
+    for i, (x, y) in enumerate(data_loader):
+        z = autoencoder.encoder(x.to(device))
+        z = z.to('cpu').detach().numpy()
+
+        plt.scatter(z[:,0], z[:, 1], c=y, cmap='Oranges')
+        plt.title("Latent space")
+        plt.xlabel("Latent variable 1")
+        plt.ylabel("Latent variable 2")
+    # Colorbar legend
+    cbar = plt.colorbar()
+
+    cbar.set_label('Location of peak (c)', rotation=270-180)
+    # place cbar label on left side of colorbar
+    cbar.ax.yaxis.set_label_coords(-1, 0.5)
+    plt.tight_layout()
+    plt.show()
+
 
 if __name__ == "__main__":
     # Load model file
@@ -116,6 +192,6 @@ if __name__ == "__main__":
     test_loader = torch.utils.data.DataLoader(dset_test, batch_size=batch_size, pin_memory=cuda)
 
     # Plot the training and validation loss
-    plot_loss(epochs, train_losses, test_losses)
+    plot_loss(train_losses, test_losses)
     plot_latent_space(autoencoder, test_loader)
     plot_reconstructions(autoencoder, test_loader)
