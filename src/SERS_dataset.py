4aec3bca6874db60cc7d9df24326847f69107ec9
Andreas Fiehn <andreas@lyhnefiehn.dk>
2023-04-12 21:06:06 +0200

    Updated dataset to contain model parameters

--- src/SERS_dataset.py
+++ src/SERS_dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch as torch
+import numpy as np
 
 class IterDataset(torch.utils.data.IterableDataset):
     def __init__(self, generator):
@@ -24,3 +25,18 @@ class SERSDataset(torch.utils.data.Dataset):
     
     def __getitem__(self, idx):
         return self.X_train[idx], self.y_train[idx]
+
+
+class SERSDatasetNPY(torch.utils.data.Dataset):
+    def __init__(self,file_name, file_name_labels=None):
+        X = np.load(file_name)
+        self.X = torch.tensor(X, dtype=torch.float32)
+
+        y = np.load(file_name_labels)
+        self.y = torch.tensor(y, dtype=torch.float32)
+
+    def __len__(self):
+        return self.X.shape[0]
+    
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[:,idx] 
